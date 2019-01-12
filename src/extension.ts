bb662137b0b5fc04ccebeaac4bcc66b837a931c0
IThawk <lianwei1994@163.com>
2019-01-12 11:20:17 +0800

    说明

--- src/extension.ts
+++ src/extension.ts
@@ -3,25 +3,32 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// 这个是vscode插件命令的入口
 export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "firstproject" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
+    // -----------------------------------
+    //   这里就是命令的注册位置，这里注册的命令就是 extension.sayHello  对应package.json中的 "activationEvents": [
+    //     "onCommand:extension.sayHello"
+    // ],
+    // "main": "./out/extension",
+    // "contributes": {
+    //     "commands": [
+    //         {
+    //             "command": "extension.sayHello",
+    //             "title": "Hello World"
+    //         }
+    //     ]
+    // },
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
+        //    这个命令就是弹出提示“Hello World!”
         vscode.window.showInformationMessage('Hello World!');
     });
 
     context.subscriptions.push(disposable);
+    // ------------------------   中间就是完整的命令注册方法
 }
 
 // this method is called when your extension is deactivated
